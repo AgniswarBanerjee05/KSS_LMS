992493c1c5d9d7b0bfa580e68582c3c22ca44886
AgniswarBanerjee91 <agniswar.banerjee.fiem.ece21@teamfuture.in>
2024-12-07 21:40:29 +0530

    Configure STATIC_ROOT for deployment

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_@876m&g2$*55!90p5cvqfsb)_f07n#33vhp2^3ggabcx#zyjr'
+SECRET_KEY = 'dzn9-26(=j_p-pyj=j@v#a@eht*zs%^+!=u@e%ep0c5qk2yc*t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -158,7 +158,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
