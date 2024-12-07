f6706820ea893ccd4d1ad84e0269ce6f3490823f
AgniswarBanerjee91 <agniswar.banerjee.fiem.ece21@teamfuture.in>
2024-12-07 22:15:59 +0530

    Configure WhiteNoise for static file handling

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -158,8 +159,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 STATICFILES_DIRS = [
